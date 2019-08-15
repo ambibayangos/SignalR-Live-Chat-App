5725d7585751412fd6e1798d226b6a836cda32aa
Arturo Sotelo Bayangos <ambibayangos@gmail.com>
2019-08-16 04:28:53 +1200

    inital commit

---
+++ SignalRChat/Hubs/ChatHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalRChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+
+        public async Task BrodCast()
+        {
+            await Clients.All.SendAsync("LogMessage");
+        }
+
+        public async Task UpdatePlayingVideo( string URL , string TITLE)
+        {
+            await Clients.All.SendAsync("Update" , URL , TITLE);
+        }
+
+
+    }
+}
